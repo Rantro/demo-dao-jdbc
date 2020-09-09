3a99079bade793da02f2a32bc91d5cc2d21a9707
germanotroian01 <germanotroian@gmail.com>
2020-09-09 20:49:30 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,11 @@ public class Program {
 		  Seller newSeller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4000.0, department);
 		  sellerDao.insert(newSeller);
 		  System.out.println("Insersed! new Id = " + newSeller.getId());
+		  
+		System.out.println("\n=== TEST 5: Seller UpDate ===");  
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 }
